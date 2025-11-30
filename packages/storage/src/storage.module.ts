76163c42cde4d0d579307ae8e9273ee79c1e3bb2
Furkan Erkara <wcnrny@outlook.com.tr>
2025-11-30 21:15:58 +0300

    feat: Implement S3 Bucket Package with AWS S3 Client SDK

--- packages/storage/src/storage.module.ts
+++ packages/storage/src/storage.module.ts
@@ -1,24 +1,30 @@
 import { Module, Global } from "@nestjs/common";
 import { S3Client } from "@aws-sdk/client-s3";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import type { AppConfig } from "@luxly/config";
 
 export const S3_CLIENT_TOKEN = "S3_CLIENT";
 
 @Global()
 @Module({
+  imports: [ConfigModule],
   providers: [
+    ConfigService,
     {
       provide: S3_CLIENT_TOKEN,
-      useFactory: () => {
+      useFactory: (configService: ConfigService<AppConfig, true>) => {
         return new S3Client({
-          region: process.env.S3_REGION || "us-east-1",
-          endpoint: process.env.S3_ENDPOINT || "http://localhost:9000",
+          region: configService.getOrThrow("S3_REGION") || "us-east-1",
+          endpoint:
+            configService.getOrThrow("S3_ENDPOINT") || "http://localhost:9000",
           credentials: {
-            accessKeyId: process.env.S3_ACCESS_KEY!,
-            secretAccessKey: process.env.S3_SECRET_KEY!,
+            accessKeyId: configService.getOrThrow("S3_ACCESS_KEY"),
+            secretAccessKey: configService.getOrThrow("S3_SECRET_KEY"),
           },
           forcePathStyle: true,
         });
       },
+      inject: [ConfigService],
     },
   ],
   exports: [S3_CLIENT_TOKEN],
