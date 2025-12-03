07d5f6ff9c59c75a56c4325565f37cf9dbb36872
Furkan <contact@wcnrny.tr>
2025-12-03 23:35:34 +0300

    Feat/types mime types (#4)
    * fix: update phase 2
    
    * feat: Create worker, implement PDF parser
    
    * fix: Update content.prisma, Create chunk.prisma, Create Document Chunks, Prepare Prisma and Postgres for Stage 4
    
    * feat: Add support for multiple mime types such as doc, docx, mp4, webm, ...

--- packages/types/src/queue.ts
+++ packages/types/src/queue.ts
@@ -12,3 +12,23 @@ export interface ProcessFilePayload {
   mimeType: string;
   userId: string;
 }
+
+export const SUPPORTED_MIME_TYPES = {
+  MEDIA: [
+    "video/mp4",
+    "video/webm",
+    "video/x-matroska",
+    "video/quicktime",
+    "audio/mpeg",
+    "audio/wav",
+    "audio/x-m4a",
+    "audio/mp4",
+    "audio/webm",
+  ],
+  DOCUMENT: [
+    // PDF isn't included in here because we have another processor for that.
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/msword",
+  ],
+  TEXT: ["text/plain", "text/markdown", "text/csv"],
+};
